eab5c9142ec11d1959a256831e024817f63b40d7
Volfzack <ghostrabbit52@gmail.com>
2025-03-21 06:57:12 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma', "Expires"],
     credentials: true
